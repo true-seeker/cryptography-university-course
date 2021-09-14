11342fc34ce58785f9e9448f75a9d61773b92559
TrueSeeker322 <murz.kirill@yandex.ru>
2021-09-14 22:10:59 +0500

    refactor

--- main.py
+++ main.py
@@ -7,30 +7,36 @@ BLOCK_SIZE = 2
 ITERATION_COUNT = 4
 EMPTY_SYMBOL_PLACEHOLDER = ' '
 
-message = 'abobus amogus sus'
-key = 'su'
 
-binary_key = ''.join([convert_python_bin(bin(ord(i))) for i in key])
-round_keys_list = [binary_key[:8], binary_key[3:11], binary_key[6:14], binary_key[8:16]]
-encrypted_message = ''
-blocks_list = [message[i:i + BLOCK_SIZE] for i in range(0, len(message), BLOCK_SIZE)]
-if len(blocks_list[-1]) == 1:
-    blocks_list[-1] += EMPTY_SYMBOL_PLACEHOLDER
+def encrypt_message(message: str, key: str) -> str:
+    binary_key = ''.join([convert_python_bin(bin(ord(i))) for i in key])
+    round_keys_list = [binary_key[:8], binary_key[3:11], binary_key[6:14], binary_key[8:16]]
+    encrypted_message = ''
+    blocks_list = [message[i:i + BLOCK_SIZE] for i in range(0, len(message), BLOCK_SIZE)]
+    if len(blocks_list[-1]) == 1:
+        blocks_list[-1] += EMPTY_SYMBOL_PLACEHOLDER
 
-for block in blocks_list:
-    k = 0
-    first_letter_id = ord(block[0])
-    second_letter_id = ord(block[1])
-    first_letter_binary_id = convert_python_bin(bin(first_letter_id))
-    second_letter_binary_id = convert_python_bin(bin(second_letter_id))
+    for block in blocks_list:
+        k = 0
+        first_letter_id = ord(block[0])
+        second_letter_id = ord(block[1])
+        first_letter_binary_id = convert_python_bin(bin(first_letter_id))
+        second_letter_binary_id = convert_python_bin(bin(second_letter_id))
 
-    for i in range(ITERATION_COUNT):
-        first_letter_binary_id = xor(first_letter_binary_id, round_keys_list[k])
-        second_letter_binary_id = middle_string_swap(second_letter_binary_id)
+        for i in range(ITERATION_COUNT):
+            first_letter_binary_id = xor(first_letter_binary_id, round_keys_list[k])
+            second_letter_binary_id = middle_string_swap(second_letter_binary_id)
 
-        first_letter_binary_id, second_letter_binary_id = second_letter_binary_id, first_letter_binary_id
-        k += 1
-    encrypted_message += f'{chr(int(first_letter_binary_id, 2))}{chr(int(second_letter_binary_id, 2))}'
+            first_letter_binary_id, second_letter_binary_id = second_letter_binary_id, first_letter_binary_id
+            k += 1
+        encrypted_message += f'{chr(int(first_letter_binary_id, 2))}{chr(int(second_letter_binary_id, 2))}'
 
-print(f'message: {message}')
-print(f'encrypted_message: {encrypted_message}')
+    print(f'message: {message}')
+    print(f'encrypted_message: {encrypted_message}')
+    return encrypted_message
+
+
+if __name__ == '__main__':
+    input_message = 'sus amogus'
+    input_key = 'fo'
+    encrypt_message(input_message, input_key)
