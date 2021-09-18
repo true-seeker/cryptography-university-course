7fad2d1717dd4100c454ac27019bc208ddc97833
True Seeker <murz.kirill@yandex.ru>
2021-09-18 10:47:18 +0500

    decrypt implemented

--- MagmaTest.py
+++ MagmaTest.py
@@ -1,6 +1,6 @@
 import unittest
 
-from magma import Number64, CipherKey, pi_transform, modulo_addition, num_shift
+from magma import Number64, CipherKey, pi_transform, modulo_addition, left_num_shift
 
 
 class TestMagma(unittest.TestCase):
@@ -12,13 +12,18 @@ class TestMagma(unittest.TestCase):
         self.assertEqual(pi_transform(self.number.left_part), 0x233b57ee)
 
     def test_modulo_addition(self):
-        self.assertEqual(modulo_addition(self.number.left_part, self.number.right_part), 0x7530eca8)
+        self.assertEqual(modulo_addition(self.number.left_part, self.number.right_part, 2 ** 32), 0x7530eca8)
 
     def test_num_shift(self):
-        self.assertEqual(num_shift(self.number.left_part, 11), 0xe5d4c7f6)
+        self.assertEqual(left_num_shift(self.number.left_part, 11), 0xe5d4c7f6)
 
     def test_encrypt(self):
-        self.assertEqual(self.number.encrypt(self.key), 0x4ee901e5c2d8ca3d)
+        self.assertEqual(self.number.magma_crypt(self.key, False).get_as_int(), 0x4ee901e5c2d8ca3d)
+
+    def test_encrypt_decrypt(self):
+        a = self.number.magma_crypt(self.key, False)
+        b = a.magma_crypt(self.key, True)
+        self.assertEqual(b.get_as_int(), self.number.get_as_int())
 
 
 if __name__ == '__main__':
--- magma.py
+++ magma.py
@@ -1,23 +1,44 @@
+Pi = [
+    (1, 7, 14, 13, 0, 5, 8, 3, 4, 15, 10, 6, 9, 12, 11, 2),
+    (8, 14, 2, 5, 6, 9, 1, 12, 15, 4, 11, 0, 13, 10, 3, 7),
+    (5, 13, 15, 6, 9, 2, 12, 10, 11, 7, 8, 1, 4, 3, 14, 0),
+    (7, 15, 5, 10, 8, 1, 6, 13, 0, 9, 3, 14, 11, 4, 2, 12),
+    (12, 8, 2, 1, 13, 4, 15, 6, 7, 0, 10, 5, 3, 14, 9, 11),
+    (11, 3, 5, 8, 2, 15, 10, 13, 14, 1, 7, 4, 12, 9, 6, 0),
+    (6, 8, 2, 3, 9, 10, 5, 12, 1, 14, 4, 7, 11, 13, 0, 15),
+    (12, 4, 6, 2, 10, 5, 11, 9, 14, 8, 13, 7, 0, 3, 15, 1)
+]
+
+
 class CipherKey:
     def __init__(self, key: int):
         if key > 0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff or key < 0x1000000000000000000000000000000000000000000000000000000000000000:
             raise ValueError("Число не 256 бит")
 
         self.key = key
-        self.str_key = hex(key).removeprefix('0x')
+        self.str_key = hex(key).replace('0x', '')
         self.count = 0
+        self.term = 1
+
+    def __call__(self, *args, **kwargs):
+        self.term = args[0]
+        if args[0] == -1:
+            self.count = 31
+        else:
+            self.count = 0
+        return self
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.count < 24:
+        if 0 <= self.count < 24:
             result = int(self.str_key[8 * (self.count % 8):8 * (self.count % 8 + 1)], 16)
-            self.count += 1
+            self.count += self.term
             return result
         elif 24 <= self.count < 32:
             result = int(self.str_key[8 * (8 - self.count % 8 - 1):8 * (8 - self.count % 8)], 16)
-            self.count += 1
+            self.count += self.term
             return result
         else:
             raise StopIteration
@@ -33,71 +54,77 @@ class Number64:
     def __str__(self):
         return f'{hex(self.left_part)}|{hex(self.right_part)}'
 
-    def encrypt(self, cipher_key: CipherKey) -> int:
-        counter = 0
-        for round_key in cipher_key:
-            # print(counter, hex(round_key), hex(self.left_part), hex(self.right_part))
-            saved_left = self.left_part
-            saved_right = self.right_part
-            self.left_part = self.right_part
-
-            self.right_part = modulo_addition(self.right_part, round_key)
-            # print(hex(self.right_part))
-            self.right_part = pi_transform(self.right_part)  # 1108F15A
-            # print(hex(self.right_part))
-
-            self.right_part = num_shift(self.right_part, 11)  # 478ad088
-            # print(hex(self.right_part))
-            self.right_part = self.right_part ^ saved_left
-            # print(hex(self.right_part))
-            if counter == 31:
-                self.left_part = self.right_part
-                self.right_part = saved_right
-            counter += 1
-
+    def get_as_int(self) -> int:
+        """Преобразование в целое число"""
         return self.left_part * 0x10 ** 8 + self.right_part
 
+    def magma_crypt(self, cipher_key: CipherKey, is_decrypt: bool):
+        """
+        Шифрование/дешифрование
+        :param cipher_key: ключ
+        :param is_decrypt: False, если зашифрование, True, если расшифрование
+        :return: Number64
+        """
+        counter = 0
+        iter_direction = -1 if is_decrypt else 1
 
-Pi = [
-    (1, 7, 14, 13, 0, 5, 8, 3, 4, 15, 10, 6, 9, 12, 11, 2),
-    (8, 14, 2, 5, 6, 9, 1, 12, 15, 4, 11, 0, 13, 10, 3, 7),
-    (5, 13, 15, 6, 9, 2, 12, 10, 11, 7, 8, 1, 4, 3, 14, 0),
-    (7, 15, 5, 10, 8, 1, 6, 13, 0, 9, 3, 14, 11, 4, 2, 12),
-    (12, 8, 2, 1, 13, 4, 15, 6, 7, 0, 10, 5, 3, 14, 9, 11),
-    (11, 3, 5, 8, 2, 15, 10, 13, 14, 1, 7, 4, 12, 9, 6, 0),
-    (6, 8, 2, 3, 9, 10, 5, 12, 1, 14, 4, 7, 11, 13, 0, 15),
-    (12, 4, 6, 2, 10, 5, 11, 9, 14, 8, 13, 7, 0, 3, 15, 1)
-]
+        this_right = self.right_part
+        this_left = self.left_part
+
+        for round_key in cipher_key(iter_direction):
+            # print(counter, hex(round_key), hex(self.left_part), hex(self.right_part))
+            saved_left = this_left
+            saved_right = this_right
+            this_left = this_right
+
+            this_right = modulo_addition(this_right, round_key, 2 ** 32)
+            # print(hex(self.right_part))
+            this_right = pi_transform(this_right)
+            # print(hex(self.right_part))
+
+            this_right = left_num_shift(this_right, 11)
+            # print(hex(self.right_part))
+            this_right = this_right ^ saved_left
+            # print(hex(self.right_part))
+            if counter == 31:
+                this_left = this_right
+                this_right = saved_right
+            counter += 1
+
+        return Number64(this_left * 0x10 ** 8 + this_right)
 
 
 def pi_transform(number: int) -> int:
-    str_num = hex(number).removeprefix('0x')
+    """Преобразование пи"""
+    str_num = hex(number).replace('0x', '')
     str_num = (8 - len(str_num)) * '0' + str_num
 
     result = ''
     for i in range(len(str_num)):
-        result += hex(Pi[i][int(str_num[i], 16)]).removeprefix('0x')
+        result += hex(Pi[i][int(str_num[i], 16)]).replace('0x', '')
     return int(result, 16)
 
 
-def num_shift(number: int, value: int) -> int:
-    str_num = bin(number).removeprefix('0b')
+def left_num_shift(number: int, value: int) -> int:
+    """Циклический сдвиг на value знаков влево"""
+    str_num = bin(number).replace('0b', '')
     str_num = (32 - len(str_num)) * '0' + str_num
     for i in range(value):
         str_num = str_num[1:] + str_num[0]
     return int(str_num, 2)
 
 
-def modulo_addition(a: int, b: int) -> int:
-    return (a + b) % (2 ** 32)
+def modulo_addition(a: int, b: int, m: int) -> int:
+    """Сложение по модулю m"""
+    return (a + b) % m
 
 
 if __name__ == '__main__':
-    a = Number64(0xfedcba9876543210)
+    num = Number64(0xfedcba9876543210)
     full_key = CipherKey(0xffeeddccbbaa99887766554433221100f0f1f2f3f4f5f6f7f8f9fafbfcfdfeff)
-    # print(hex(pi_transform(a.left_part)))
-    # print(hex(modulo_addition(a.left_part,a.right_part)))
-    # print(hex(num_shift(a.left_part, 11)))
-    # print(hex(0x68695433 << 11))
-    # quit()
-    print(hex(a.encrypt(full_key)))
+
+    encrypted_number = num.magma_crypt(full_key, False)
+    print(encrypted_number)
+
+    decrypted_number = encrypted_number.magma_crypt(full_key, True)
+    print(decrypted_number)
