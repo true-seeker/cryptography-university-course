072d252bb11b2ee29074bbe5383aa3dfee19ada5
TrueSeeker322 <murz.kirill@yandex.ru>
2021-10-06 09:48:44 +0500

    refactoring

--- elgamal.py
+++ elgamal.py
@@ -1,15 +1,20 @@
-from Crypto.Util import number
 from functools import wraps
-from fast_power import fast_pow_module
 from math import gcd as bltin_gcd
-from hashlib import blake2b, blake2s, md5
+
+from Crypto.Util import number
+
+from fast_power import fast_pow_module
+
+LENGTH = 3
 
 
 def coprime2(a, b):
+    """Проверка """
     return bltin_gcd(a, b) == 1
 
 
 def cache_gcd(f):
+    """Декоратор для кэширования НОД"""
     cache = {}
 
     @wraps(f)
@@ -26,12 +31,14 @@ def cache_gcd(f):
 
 @cache_gcd
 def gcd(a, b):
+    """НОД"""
     while b != 0:
         a, b = b, a % b
     return a
 
 
 def prime_root(modulo):
+    """Первообразный корень"""
     coprime_set = {num for num in range(1, modulo) if gcd(num, modulo) == 1}
 
     for g in range(1, modulo):
@@ -40,7 +47,7 @@ def prime_root(modulo):
 
 
 def egcd(a: int, b: int):
-    """return (g, x, y) such that a*x + b*y = g = gcd(a, b)"""
+    """Расширенный алгоритм Евклида"""
     if a == 0:
         return (b, 0, 1)
     else:
@@ -50,7 +57,7 @@ def egcd(a: int, b: int):
 
 
 def modinv(a: int, b: int) -> int:
-    """return x such that (x * a) % b == 1"""
+    """Обратное по модулю"""
     g, x, _ = egcd(a, b)
     if g != 1:
         raise Exception('gcd(a, b) != 1')
@@ -58,6 +65,7 @@ def modinv(a: int, b: int) -> int:
 
 
 def my_hash(message, max_value):
+    """Хэширование"""
     hs = 0
     for i in str(message):
         hs += ord(i)
@@ -66,55 +74,49 @@ def my_hash(message, max_value):
     return hs
 
 
-LENGTH = 3
-
-M = 'hfgdhdfghgfd'
+M = 'hihihihihi'
 p = number.getPrime(LENGTH * 4)
-# p = 23
-print('p', p)
 
 m = my_hash(M, p - 1)
-# m = 3
-print('m', m)
 
 k = -1
 for i in range(3, p - 1):
     if coprime2(i, p - 1):
         k = i
         break
-# k = 5
-print('k', k)
 
 x = p + 1
 while x >= p - 1:
     x = number.getPrime(LENGTH)
-# x = 7
-print('x', x)
 
 g = prime_root(p)
-# g = 5
-print('g', g)
 
 y = fast_pow_module(g, x, p)
-print('y', y)
 
 r = fast_pow_module(g, k, p)
-print('r', r)
 
 k_minus_one = modinv(k, p - 1)
-print('k_minus_one', k_minus_one)
 
 s = ((m - x * r) * modinv(k, p - 1)) % (p - 1)
-print('s', s)
 
+m = my_hash(M, p - 1)
+print('p', p)
+print('m', m)
+print('k', k)
+print('x', x)
+print('g', g)
+print('y', y)
+print('r', r)
+print('k_minus_one', k_minus_one)
+print('s', s)
 print('podpis', M, r, s)
 
 # ПРОВЕРКА
-m = my_hash(M, p - 1)
-
-print('0<r<p', 0 < r < p, )
-print('0<s<p-1', 0 < s < p - 1)
-
-print(fast_pow_module(y, r, p), fast_pow_module(r, s, p), (fast_pow_module(y, r, p) * fast_pow_module(r, s, p)) % p,
-      fast_pow_module(g, m, p))
-print((fast_pow_module(y, r, p) * fast_pow_module(r, s, p)) % p == fast_pow_module(g, m, p))
+if 0 < r < p and 0 < s < p - 1 and (fast_pow_module(y, r, p) * fast_pow_module(r, s, p)) % p == fast_pow_module(g, m,
+                                                                                                                p):
+    print('Подпись успешно проверена')
+else:
+    print('0<r<p', 0 < r < p, )
+    print('0<s<p-1', 0 < s < p - 1)
+    print((fast_pow_module(y, r, p) * fast_pow_module(r, s, p)) % p == fast_pow_module(g, m, p))
+    raise Exception('Проверка не пройдена')
