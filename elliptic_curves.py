cf36d4fe3bceb4b7f0283cb842ef98c34ff6238f
TrueSeeker322 <murz.kirill@yandex.ru>
2021-11-06 14:06:18 +0500

    ECDH

--- elliptic_curves.py
+++ elliptic_curves.py
@@ -1,4 +1,3 @@
-import collections
 import random
 
 
@@ -12,17 +11,29 @@ class EllipticCurve:
         self.p = p
         self.name = name
 
+        print(name)
+
+
+class Point:
+    def __init__(self, x, y):
+        self.y = y
+        self.x = x
+
+    def __eq__(self, other):
+        if self.x == other.x and self.y == other.y:
+            return True
+        return False
+
 
 curve = EllipticCurve(
     name='secp256k1',
     p=0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f,
     a=0,
     b=7,
-    g=(0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798,
-       0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8),
+    g=Point(0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798,
+            0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8),
     n=0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141,
-    h=1,
-)
+    h=1)
 
 
 def egcd(a: int, b: int):
@@ -37,115 +48,77 @@ def egcd(a: int, b: int):
 
 def modinv(a: int, b: int) -> int:
     """Обратное по модулю"""
-    g, x, _ = egcd(a, b)
+    g, x, y = egcd(a, b)
+    if g == -1:
+        x *= -1
+        g *= -1
+
     assert g == 1
     return x % b
 
 
-def inverse_mod(k, p):
-    """Returns the inverse of k modulo p"""
-    if k == 0:
-        raise ZeroDivisionError('division by zero')
-
-    if k < 0:
-        # k ** -1 = p - (-k) ** -1  (mod p)
-        return p - inverse_mod(-k, p)
-
-    # Extended Euclidean algorithm.
-    s, old_s = 0, 1
-    t, old_t = 1, 0
-    r, old_r = p, k
-
-    while r != 0:
-        quotient = old_r // r
-        old_r, r = r, old_r - quotient * r
-        old_s, s = s, old_s - quotient * s
-        old_t, t = t, old_t - quotient * t
-
-    gcd, x, y = old_r, old_s, old_t
-
-    assert gcd == 1
-    assert (k * x) % p == 1
-
-    return x % p
-
-
-# Functions that work on curve points #########################################
-
-def is_on_curve(point):
-    """Returns True if the given point lies on the elliptic curve."""
+def is_on_curve(point: Point):
+    """Проверка принадлежности точки кривой"""
     if point is None:
-        # None represents the point at infinity.
         return True
-
-    x, y = point
+    x, y = point.x, point.y
 
     return (y * y - x * x * x - curve.a * x - curve.b) % curve.p == 0
 
 
-def point_neg(point):
-    """Returns -point."""
+def point_neg(point: Point):
+    """Нахождение обратной точки"""
     assert is_on_curve(point)
 
     if point is None:
-        # -0 = 0
         return None
 
-    x, y = point
-    result = (x, -y % curve.p)
+    x, y = point.x, point.y
+    result = Point(x, -y % curve.p)
 
     assert is_on_curve(result)
 
     return result
 
 
-def point_add(point1, point2):
-    """Returns the result of point1 + point2 according to the group law."""
+def point_add(point1: Point, point2: Point):
+    """Сложение двух точек"""
     assert is_on_curve(point1)
     assert is_on_curve(point2)
 
     if point1 is None:
-        # 0 + point2 = point2
         return point2
     if point2 is None:
-        # point1 + 0 = point1
         return point1
 
-    x1, y1 = point1
-    x2, y2 = point2
+    x1, y1 = point1.x, point1.y
+    x2, y2 = point2.x, point2.y
 
     if x1 == x2 and y1 != y2:
-        # point1 + (-point1) = 0
         return None
 
     if x1 == x2:
-        # This is the case point1 == point2.
-        m = (3 * x1 * x1 + curve.a) * inverse_mod(2 * y1, curve.p)
+        m = (3 * x1 * x1 + curve.a) * modinv(2 * y1, curve.p)
     else:
-        # This is the case point1 != point2.
-        print(inverse_mod(x1 - x2, curve.p))
-        print(modinv(x1 - x2, curve.p))
-        m = (y1 - y2) * inverse_mod(x1 - x2, curve.p)
+        m = (y1 - y2) * modinv(x1 - x2, curve.p)
 
     x3 = m * m - x1 - x2
     y3 = y1 + m * (x3 - x1)
-    result = (x3 % curve.p,
-              -y3 % curve.p)
+    result = Point(x3 % curve.p, -y3 % curve.p)
 
     assert is_on_curve(result)
 
     return result
 
 
-def scalar_mult(k, point):
-    """Returns k * point computed using the double and point_add algorithm."""
+def scalar_mult(k, point: Point):
+    """Умножение k на точку"""
     assert is_on_curve(point)
 
     if k % curve.n == 0 or point is None:
         return None
 
     if k < 0:
-        # k * point = -k * (-point)
         return scalar_mult(-k, point_neg(point))
 
     result = None
@@ -153,10 +126,8 @@ def scalar_mult(k, point):
 
     while k:
         if k & 1:
-            # Add.
             result = point_add(result, addend)
 
-        # Double.
         addend = point_add(addend, addend)
 
         k >>= 1
@@ -166,30 +137,22 @@ def scalar_mult(k, point):
     return result
 
 
-# Keypair generation and ECDHE ################################################
-
 def make_keypair():
-    """Generates a random private-public key pair."""
     private_key = random.randrange(1, curve.n)
     public_key = scalar_mult(private_key, curve.g)
 
     return private_key, public_key
 
 
-print('Curve:', curve.name)
-
-# Alice generates her own keypair.
 alice_private_key, alice_public_key = make_keypair()
-print(f"Alice's private key: {hex(alice_private_key)}")
-print(f"Alice's public key: ({hex(alice_public_key[0])}, {hex(alice_public_key[0])})")
+print(f"Закрытый ключ А: {hex(alice_private_key)}")
+print(f"Открытый ключ А: ({hex(alice_public_key.x)}, {hex(alice_public_key.y)})\n")
 
-# Bob generates his own key pair.
 bob_private_key, bob_public_key = make_keypair()
-print(f"Bob's private key: {hex(bob_private_key)}")
-print(f"Bob's public key: ({hex(bob_public_key[0])}, {hex(bob_public_key[1])})")
+print(f"Закрытый ключ B: {hex(bob_private_key)}")
+print(f"Открытый ключ B: ({hex(bob_public_key.x)}, {hex(bob_public_key.y)})\n")
 
-# Alice and Bob exchange their public keys and calculate the shared secret.
 s1 = scalar_mult(alice_private_key, bob_public_key)
 s2 = scalar_mult(bob_private_key, alice_public_key)
 assert s1 == s2
-print(f'Shared secret: ({hex(s1[0])}, {hex(s1[1])})')
+print(f'Вычисленный секрет от А и от Б: ({hex(s1.x)}, {hex(s1.y)})')
