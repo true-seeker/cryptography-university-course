d801c059f8e0a796bcc6499a973cf38841ac68bc
Kirill Murzin <murz.kirill@yandex.ru>
2022-06-19 23:05:31 +0500

    debtor_creditor_untraceability

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import re
 from functools import reduce
 from math import gcd
 from random import randrange
-from Crypto.Util import number
+
+
+# from Crypto.Util import number
 
 
 def isprime(n):
@@ -44,44 +46,65 @@ def coprime(a, b):
     return gcd(a, b) == 1
 
 
+def calculate_h_list(val):
+    h_list = []
+    primes = gen_prime(3, val ** 2)
+    for digit, prime in zip(bin(val)[2:][::-1], primes):
+        if digit == '1':
+            h_list.append(prime)
+    return h_list
+
+
 class Bill:
     def __init__(self, value):
         self.value = value
         self.h_list = []
 
-        primes = gen_prime(3, self.value)
-        for digit, prime in zip(bin(self.value)[2:][::-1], primes):
-            if digit == '1':
-                self.h_list.append(prime)
-
-        print(self.h_list)
+        self.h_list = calculate_h_list(self.value)
         self.h = reduce(lambda a, b: a * b, self.h_list)
-        print(self.h)
-        print(pow(self.h, -1, n))
+        print(f'Купюра {value}: h = {self.h_list} = {self.h}\n')
 
     def blind_sign(self, val):
         # oh = pow(self.h, -1, (p - 1) * (q - 1))
         #
         # print(pow(val, pow(self.h, -1, n), n))
-        return pow(val, pow(self.h, -1, (p - 1) * (q - 1)), (p - 1) * (q - 1))
+        # return pow(val, pow(self.h, -1, (p - 1) * (q - 1)), (p - 1) * (q - 1))
+
+        return pow(val, pow(self.h, -1, fn), fn)
 
 
 if __name__ == '__main__':
     # p = rand_prime(10, 50)
     # q = rand_prime(10, 50)
     p = 47
-    q = 17  # r1, r2 =23
-    while not coprime(p, q):
-        p = rand_prime(500, 5000)
-        q = rand_prime(500, 5000)
+    q = 17  # r1, r2 = 23
+    # while not coprime(p, q):
+    #     p = rand_prime(500, 5000)
+    #     q = rand_prime(500, 5000)
     # p = 509
     # q = 827
     print(f'p: {p}')
     print(f'q: {q}')
+    # n = (p - 1) * (q - 1)
     n = p * q
-
+    fn = (p - 1) * (q - 1)
+    print(f'n: {n}')
     # разряд числа
     value_digit = 4
 
     bill = Bill(15)
-    bill.blind_sign(304339)
+
+    number1 = 153
+
+    blind_signed_number1 = bill.blind_sign(number1)
+    print(f'Шаг 2:\nblind_signed_number1: {blind_signed_number1}\n')
+
+    shag4_number1 = 402
+    shag4_number2 = 722
+
+    bill2 = Bill(5)
+
+    # blind_signed_number_t = bill2.blind_sign(shag4_number2)
+    blind_signed_number_t = pow(shag4_number2, pow(bill2.h, -1, fn), n)
+
+    print(f'Шаг 5:\nblind_signed_number_t: {blind_signed_number_t}')
